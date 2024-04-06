ed873db3d08e62d3bb7709bf9ff6253db19a09b8
Shushil Gautam <mr.kaizectric@gmail.com>
2024-04-06 09:11:15 +0545

    completed

--- Eventdriven opengl.py
+++ Eventdriven opengl.py
@@ -1,23 +1,39 @@
 import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import numpy as np
 
 window_width = 800
 window_height = 600
+
+points = []
+line_width = 5.0  # Initial line width
 
 def init():
     glClearColor(1.0, 1.0, 1.0, 1.0)
     glEnable(GL_DEPTH_TEST)
 
-def draw_polygon(vertices):
-    glBegin(GL_POLYGON)
-    for vertex in vertices:
-        glVertex2f(*vertex)
+def draw_lines():
+    glColor3f(0.0, 0.0, 1.0)  # Blue color for lines
+    glLineWidth(line_width)  # Set line width
+    glBegin(GL_LINES)
+    for i in range(0, len(points), 2):
+        glVertex2f(*points[i])
+        if i + 1 < len(points):
+            glVertex2f(*points[i + 1])
     glEnd()
 
-def key_callback(window, key, scancode, action, mods):
-    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
-        glfw.set_window_should_close(window, True)
+def mouse_button_callback(window, button, action, mods):
+    global points
+    if button == glfw.MOUSE_BUTTON_LEFT and action == glfw.PRESS:
+        xpos, ypos = glfw.get_cursor_pos(window)
+        xpos = 2 * xpos / window_width - 1
+        ypos = 1 - 2 * ypos / window_height
+        points.append((xpos, ypos))
+        if len(points) % 2 == 0:
+            points.append((xpos, ypos))
+
+
 
 def main():
     if not glfw.init():
@@ -29,15 +45,26 @@ def main():
         return
 
     glfw.make_context_current(window)
-    glfw.set_key_callback(window, key_callback)
+    glfw.set_mouse_button_callback(window, mouse_button_callback)
+    
 
     init()
 
-    draw_polygon([(500, 100), (700, 100), (700, 300), (600, 400),(500,300)])
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
 
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluOrtho2D(-1, 1, -1, 1)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        draw_lines()
+
+        glfw.swap_buffers(window)
 
     glfw.terminate()
 
 if __name__ == "__main__":
     main()
-
